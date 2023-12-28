bbab368d8917dc987c4379c5baec53f8d8fcd34c
TheshikaSamaraweera <152192209+TheshikaSamaraweera@users.noreply.github.com>
2023-12-28 14:45:25 +0530

    login+signin

---
+++ lib/pages/login.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:workapp/pages/signup.dart';
+import 'package:workapp/widget/widget_support.dart';
+
+class LogIn extends StatefulWidget{
+  const LogIn ({super.key});
+  @override
+  State<LogIn> createState() => _LogInState();
+
+}
+class _LogInState extends State<LogIn>{
+  int currentTabIndex=0;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        child: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height:MediaQuery.of(context).size.height/2.5,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFFff5c30),
+                  Color(0xFFe74b1a),
+                ])),
+      ),
+          Container(
+            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/3),
+            height: MediaQuery.of(context).size.height/2,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40)
+                ) ),
+            child: Text(""),
+
+      ),
+         Container(
+           margin: EdgeInsets.only(top: 60.0,left: 20.0,right: 20.0),
+
+             child: Column(children: [
+             Center(child: Image.asset("images/logo.png",
+               width: MediaQuery.of(context).size.width / 1.5,
+               fit: BoxFit.cover,
+             )),
+                SizedBox(height: 50.0,),
+      Material(   //login box 1
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          padding: EdgeInsets.only(left: 20.0, right: 20.0),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height / 1.6,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20)),
+          child: Column(children: [
+              SizedBox(height: 30.0,),
+            Text(
+              "Login", style: AppWidget.boldTextFieldstyle(),),
+            SizedBox(height: 30.0,),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: AppWidget.SemiboldTextFieldstyle(),
+                  prefixIcon: Icon(Icons.email_outlined)),
+            ),
+            SizedBox(height: 30.0,),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: AppWidget.SemiboldTextFieldstyle(),
+                  prefixIcon: Icon(Icons.password_outlined)),
+            ),
+            SizedBox(height: 20.0,),
+            Container(
+              alignment: Alignment.topRight,
+              child: Text("Forgot Password?",
+                style: AppWidget.SemiboldTextFieldstyle(),),),
+            SizedBox(height: 40.0,),
+            Material(
+              elevation: 5.0,
+                borderRadius: BorderRadius.circular(20),
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical:8.0),
+                width: 200,
+                height: 35,
+                decoration: BoxDecoration(
+                    color: Color(0Xffff5722),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                    child: Text("LOGIN",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontFamily: 'Poppins1',
+                          fontWeight: FontWeight.bold),
+                    )),
+              ),
+            ),
+             SizedBox(height: 20.0,),
+            GestureDetector(
+                onTap:(){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUp()));
+                } ,
+                child: Text("Don't have account? Sign up",style:AppWidget.SemiboldTextFieldstyle() ,))
+
+
+
+              ],),
+             ),
+            )
+           ],
+          ),
+        )
+       ],
+      ),
+     ),
+    );
+  }
+}
