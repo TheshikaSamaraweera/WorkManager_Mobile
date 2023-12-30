c4d506c4c0923a148d9d5be4d72c7e2f3877bd45
TheshikaSamaraweera <152192209+TheshikaSamaraweera@users.noreply.github.com>
2023-12-30 12:53:18 +0530

    role auth

---
+++ lib/student.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: use_build_context_synchronously, prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class Student extends StatefulWidget {
+  const Student({super.key});
+
+  @override
+  State<Student> createState() => _StudentState();
+}
+
+class _StudentState extends State<Student> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Student Dashbord"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              logout(context);
+            },
+            icon: Icon(
+              Icons.logout,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    CircularProgressIndicator();
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
+      ),
+    );
+  }
+}
---
+++ lib/teacher.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class Teacher extends StatefulWidget {
+  const Teacher({super.key});
+
+  @override
+  State<Teacher> createState() => _TeacherState();
+}
+
+class _TeacherState extends State<Teacher> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Admin Dashboard"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              logout(context);
+            },
+            icon: Icon(
+              Icons.logout,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    CircularProgressIndicator();
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
+      ),
+    );
+  }
+}
