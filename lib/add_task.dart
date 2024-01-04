6cae013b1c9477b84f486d80964405b07d73d0f5
Charith Lakshan <139710239+charith3903@users.noreply.github.com>
2024-01-04 16:17:11 +0530

    to do list
    over

--- lib/add_task.dart
+++ lib/add_task.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class AddTaskPage extends StatefulWidget {
   @override
@@ -7,6 +9,8 @@ class AddTaskPage extends StatefulWidget {
 
 class _AddTaskPageState extends State<AddTaskPage> {
   TextEditingController _taskController = TextEditingController();
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,17 +29,32 @@ class _AddTaskPageState extends State<AddTaskPage> {
               decoration: InputDecoration(labelText: 'Task'),
             ),
             SizedBox(height: 16.0),
+            TextField(
+              controller: _dateController,
+              decoration: InputDecoration(labelText: 'Date (YYYY-MM-DD)'),
+              readOnly: true,
+              onTap: () => _selectDate(context),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _timeController,
+              decoration: InputDecoration(labelText: 'Time (HH:mm)'),
+            ),
+            SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () {
-                // Implement the logic to add the task
+              onPressed: () async {
                 String task = _taskController.text;
-                // Perform any additional validation or processing here
+                String date = _dateController.text;
+                String time = _timeController.text;
 
-                // Example: Saving the task to a list or database
-                // tasks.add(task);
+                await FirebaseFirestore.instance.collection('tasks').add({
+                  'taskName': task,
+                  'date': date,
+                  'time': time,
+                });
 
-                // Navigator.pop to go back to the previous page
-                Navigator.pop(context, task);
+                // Navigate back to the previous screen
+                Navigator.of(context).pop();
               },
               child: Text('Add Task'),
             ),
@@ -43,5 +62,18 @@ class _AddTaskPageState extends State<AddTaskPage> {
         ),
       ),
     );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (picked != null) {
+      _dateController.text = picked.toLocal().toString().split(' ')[0];
+    }
   }
 }
