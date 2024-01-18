1e91b533a6e4e5430cf7c5a7c337b84898305fbf
Charith Lakshan <139710239+charith3903@users.noreply.github.com>
2024-01-18 18:32:23 +0530

    time table

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primaryColor: Colors.blue[900],
       ),
-      home: TimetablePage(),
+      home:Register()
     );
   }
 }
